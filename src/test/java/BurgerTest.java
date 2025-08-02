355f106b6e85779bdfc5b0b5e61c157f7cccda5d
holod <holodulinn@mail.ru>
2025-08-02 13:14:26 +0400

    Тест BurgerTest разделен на 3 отдельных теста

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,9 +16,8 @@ public class BurgerTest {
     @Mock
     private Ingredient ing2;
 
-    @Test
-    public void burgerMoveAndRemoveIngredientTest(){
-
+    @BeforeEach
+    public void PreReq(){
         Mockito.when(bun.getName()).thenReturn("Bulka");
         Mockito.when(bun.getPrice()).thenReturn(10.0f);
 
@@ -29,6 +29,11 @@ public class BurgerTest {
         Mockito.when(ing2.getType()).thenReturn(IngredientType.FILLING);
         Mockito.when(ing2.getPrice()).thenReturn(50.0f);
 
+
+    }
+
+    @Test
+    public void burgerAddIngredientTest(){
         Burger burger = new Burger();
         burger.setBuns(bun);
         burger.addIngredient(ing1);
@@ -37,15 +42,30 @@ public class BurgerTest {
         String[] lines = burger.getReceipt().split("\n");
         Assertions.assertEquals("= sauce Ketchup =", lines[1].trim());
         Assertions.assertEquals("= filling Meat =", lines[2].trim());
+    }
+    @Test
+    public void burgerMoveIngredientTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ing1);
+        burger.addIngredient(ing2);
 
         burger.moveIngredient(0,1);
-        lines = burger.getReceipt().split("\n");
+        String[] lines = burger.getReceipt().split("\n");
         Assertions.assertEquals("= filling Meat =", lines[1].trim());
         Assertions.assertEquals("= sauce Ketchup =", lines[2].trim());
+    }
+    @Test
+    public void burgerRemoveIngredientTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ing1);
+        burger.addIngredient(ing2);
+        String[] lines = burger.getReceipt().split("\n");
 
         burger.removeIngredient(1);
         lines = burger.getReceipt().split("\n");
-        Assertions.assertEquals("= filling Meat =", lines[1].trim());
+        Assertions.assertEquals("= sauce Ketchup =", lines[1].trim());
         Assertions.assertEquals("(==== Bulka ====)", lines[2].trim());
     }
 }
